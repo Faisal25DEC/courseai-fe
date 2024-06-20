19c53dd97aad5980dc7376bded4ce6f9499a9878
Faisal25DEC <faisalmd25121999@gmail.com>
2024-06-21 02:35:35 +0530

    using gateway upload

--- services/file.service.ts
+++ services/file.service.ts
@@ -2,10 +2,13 @@ export const FileService = {
   uploadImage: (file: any) => {
     const formData = new FormData();
     formData.append("file", file);
-    return fetch("/api/upload/image", {
-      method: "POST",
-      body: formData,
-    })
+    return fetch(
+      "https://interactai-gateway-dev.onrender.com/api/v1/upload-pdf",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
       .then((response) => response.json())
       .then((data) => {
         return data;

9a0924893d3bf6186c4c1cb8844490b9b5e5b2af
Faisal25DEC <faisalmd25121999@gmail.com>
2024-06-26 19:39:11 +0530

    fix:sign-in and sign-up styling

--- app/(settings)/settings/page.tsx
+++ app/(settings)/settings/page.tsx
@@ -5,7 +5,7 @@ import { Icon } from "@iconify/react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col gap-2 h-full w-[100%] mx-auto py-8">
+    <div className="flex flex-col gap-2 h-full w-[100%] mx-auto ">
       <div className="flex w-[90%] m-auto justify-between items-center py-8">
         <div>
           <h1 className=" font-normal text-gray-600 text-2xl">Settings</h1>

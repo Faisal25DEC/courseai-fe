b51cf7f426917268476b240a678f747c6b2aaf24
Faisal25DEC <faisalmd25121999@gmail.com>
2024-06-26 18:10:20 +0530

    settings ui + favico

--- app/(settings)/settings/page.tsx
+++ app/(settings)/settings/page.tsx
@@ -1,13 +1,22 @@
+"use client";
 import { OrganizationProfile } from "@clerk/nextjs";
-import React from "react";
+import React, { useEffect } from "react";
 import { Icon } from "@iconify/react";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className="flex h-full w-[90%] mx-auto justify-center items-center py-8">
-      <OrganizationProfile routing="virtual" />
+    <div className="flex flex-col gap-2 h-full w-[100%] mx-auto py-8">
+      <div className="flex w-[90%] m-auto justify-between items-center py-8">
+        <div>
+          <h1 className=" font-normal text-gray-600 text-2xl">Settings</h1>
+        </div>
+      </div>
+      <hr className="bg-gray-600 w-full" />
+      <div className="w-[90%] mx-auto">
+        <OrganizationProfile routing="virtual" />
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Page;

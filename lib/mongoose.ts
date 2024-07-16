2590a27f038b10bbb27cbf9007129d87682ebb7a
kartik126 <lavi9921@gmail.com>
2024-07-17 00:38:26 +0530

    fix:mongo string env name

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL as string;
+const MONGODB_URL = process.env.NEXT_PUBLIC_MONGODB_URL as string;
 
 if (!MONGODB_URL) {
   throw new Error('Please define the MONGODB_URL environment variable inside .env.local');
